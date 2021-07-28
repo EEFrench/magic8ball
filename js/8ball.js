71f28677fe7a1afe85f3df6d8d0ab93fdd078679
EEFrench <edward.e.french@gmail.com>
2021-07-28 16:48:34 -0700

    Renamed images

--- js/8ball.js
+++ js/8ball.js
@@ -1,8 +1,7 @@
-
-
+//ADD AND SIZE IMAGE TO HTML DOC
 const img = document.createElement("img");
-img.width=500
-img.src = "../img/magic8ball_start.png";
+img.width=500 //SETS IMAGE SIZE
+img.src = "../img/01.png"; //SELECTS IMAGE
 const src = document.getElementById("ball");
 src.appendChild(img);
 
