6a33f295caf3905656ef22fee1af210b890a6c7a
EEFrench <edward.e.french@gmail.com>
2021-07-28 17:43:19 -0700

    Add js codes

--- js/8ball.js
+++ js/8ball.js
@@ -1,7 +1,60 @@
 //ADD AND SIZE IMAGE TO HTML DOC
-const img = document.createElement("img");
+let img = document.createElement("img");
 img.width=500 //SETS IMAGE SIZE
 img.src = "../img/01.png"; //SELECTS IMAGE
-const src = document.getElementById("ball");
+let src = document.getElementById("ball");
 src.appendChild(img);
 
+//GET IMAGES
+image_array = [
+    '01.png',
+    '02.png',
+    '03.png',
+    '04.png',
+    '05.png',
+    '06.png',
+    '07.png',
+    '08.png',
+    '09.png',
+    '10.png',
+    '11.png',
+    '12.png',
+    '13.png',
+    '14.png',
+    '15.png',
+    '16.png',
+    '17.png',
+    '18.png',
+    '19.png',
+    '20.png',
+    '21.png',
+    '22.png',
+    '23.png'
+]
+
+// function eightBall(){
+//     //GET RANDOM ARRAY
+//     random_answer = Math.floor(Math.random() * image_array.length);
+//     //SELECT IMAGE
+//     answer = image_array[random_answer]
+//     document.getElementById("ball").src = `./img/${answer}`
+// }
+
+// // Get all the images
+// image_array = [
+//     '1.jpg',
+//     '2.jpg',
+//     '3.jpg',
+//     '4.jpg'
+//   ]
+  
+  function get_random_image(){
+    // Get a random index
+    random_index = Math.floor(Math.random() * image_array.length);
+  
+    // Get an image at the random_index
+    selected_image = image_array[random_index]
+  
+    // Display the image
+    document.getElementById('image_shower').src = `../img/${selected_image}`
+  }
