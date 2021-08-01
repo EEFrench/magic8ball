5937da2685694081e9175fafb2ba243fca89f157
EEFrench <edward.e.french@gmail.com>
2021-07-31 18:02:40 -0700

    Adjust js to fix image issues try 1

--- js/8ball.js
+++ js/8ball.js
@@ -1,65 +1,68 @@
 //INSERT NAME INTO TOPBAR
-const Magic = document.getElementById('MagicAnswer');
-Magic.innerText = "Majyk 8 Ball";
+let Magic = document.getElementById('MagicAnswer');
+Magic.innerText = "The Majyk 8 Ball";
 
 //ADD QUESTION INPUT
 // const question = document.createElement('input');
 // document.body.appendChild(question);
 //const input.getElementById(input);
 
-const form = document.createElement('form');
+let form = document.createElement('form');
 document.body.appendChild(form);
 
+//ADD INPUT AND BUTTON
+let questionInput = addInput('input', 'ASK');
+let submitButton = addButton('Do you feel Lucky?');
 
-const usernameInput = addInput('input', 'ASK');
-const submitButton = addButton('Do you feel Lucky?');
 
-const userInfoElement = document.createElement('div');
-const userNameElement = document.createElement('h1');
-const phoneNumberElement = document.createElement('p');
-const emailElement = document.createElement('p');
-const restartButton = document.createElement('button');
+
+//ADD ELEMENTS TO DISPLAY QUESTION
+let userInfoElement = document.getElementById('input');
+let userNameElement = document.createElement('h1');
+let restartButton = document.createElement('button');
 document.body.appendChild(userInfoElement);
 
+//CREATE RESTART BUTTON
 restartButton.innerHTML = 'Restart';
 
+//DISPLAY RESTART BUTTON
 userInfoElement.appendChild(userNameElement);
 userInfoElement.appendChild(restartButton);
 userInfoElement.style.display = 'none';
 
+
 restartButton.addEventListener('click', function() {
     toggleFormUserInfo();
     setInputValues('', '', '');
+
 })
-
-
-
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
-  const username = form[0].value;
+  let username = form[0].value;
   displayUserInfo(username);
   toggleFormUserInfo();
+  get_random_image();
 })
 
 function addInput(type, placeholder) {
-  const input = document.createElement(type);
+  let input = document.createElement(type);
   form.appendChild(input);
   input.placeholder = placeholder;
 }
 
 function addButton(text){
-  const button = document.createElement('button');
+  let button = document.createElement('button');
   form.appendChild(button);
   button.innerHTML = text;
   return button;
 }
 
-function displayUserInfo(username, email, phoneNumber) {
+function displayUserInfo(username) {
   userNameElement.innerText = username;
 }
 
-function setInputValues(username, email, phoneNumber) {
+function setInputValues(username) {
   form[0].value = username;
 }
 
@@ -77,9 +80,9 @@ function toggleFormUserInfo() {
 
 //ADD INITIAL IMAGE TO HTML DOC
 let img = document.createElement("img");
-img.src = "../img/01.png"; //SELECTS IMAGE
-let src = document.getElementById("ball");
-src.appendChild(img);
+img.src = "../img/01.png" //SELECTS IMAGE
+//let src = document.getElementById("ball");
+//src.appendChild(img);
 
 //ARRAY OF IMAGE IDs
 ballImage_array = [
@@ -112,11 +115,10 @@ ballImage_array = [
   function get_random_image(){
     // GET RANDOM NUMBER
     random_index = Math.floor(Math.random() * ballImage_array.length);
-  
     // GET RANDOM BALL IMAGE
     selected_image = ballImage_array[random_index];
-  
     // DISPLAY BALL IMAGE
     document.getElementById('image_shower').src = `../img/${selected_image}`
+
   }
 
