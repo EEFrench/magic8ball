e1785129a3d0536603bfff30853fa895c51287c8
EEFrench <edward.e.french@gmail.com>
2021-08-02 21:42:32 -0700

    js img Adjust js to fix image issues try 13

--- js/8ball.js
+++ js/8ball.js
@@ -23,60 +23,6 @@ questionElement.appendChild(repeatQuestion);
 questionElement.appendChild(restartButton);
 questionElement.style.display = 'none';
 
-//RESTART FUNCTION
-restartButton.addEventListener('click', function() {
-    toggleFormUserInfo();
-    setInputValues('', '', '');
-    removeImage();
-})
-
-//SUBMIT QUESTION FUNCTION
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  let question = form[0].value;
-  displayUserInfo(question);
-  toggleFormUserInfo();
-  get_random_image();
-})
-
-//ADD INPUT FUNCTION
-function addInput(type, placeholder) {
-  let input = document.createElement(type);
-  form.appendChild(input);
-  input.placeholder = placeholder;
-}
-
-//ADD BUTTON FUNCTION
-function addButton(text){
-  let button = document.createElement('button');
-  form.appendChild(button);
-  button.innerHTML = text;
-  return button;
-}
-
-//DISPLAY QUESTION ON SCREEN
-function displayUserInfo(question) {
-  repeatQuestion.innerText = question;
-}
-
-
-function setInputValues(question) {
-  form[0].value = question;
-}
-
-
-function toggleFormUserInfo() {
-
-  if (form.style.display === 'none') {
-      form.style.display = 'block';
-      questionElement.style.display = 'none';
-  } else {
-      form.style.display = 'none';
-      questionElement.style.display = 'block';
-  }
- 
-}
-
 //ARRAY OF IMAGE IDs
 ballImage_array = [
     '../img/03.png',
@@ -101,44 +47,72 @@ ballImage_array = [
     '../img/22.png',
     '../img/23.png'
 ]
-// ballImage_array = [
-//   '03.png',
-//   '04.png',
-//   '05.png',
-//   '06.png',
-//   '07.png',
-//   '08.png',
-//   '09.png',
-//   '10.png',
-//   '11.png',
-//   '12.png',
-//   '13.png',
-//   '14.png',
-//   '15.png',
-//   '16.png',
-//   '17.png',
-//   '18.png',
-//   '19.png',
-//   '20.png',
-//   '21.png',
-//   '22.png',
-//   '23.png'
-// ]
+
 
 //GET IMAGE FUNCTION
 function get_random_image(){
-  // GET RANDOM NUMBER
-  random_index = Math.floor(Math.random() * ballImage_array.length);
-  // GET RANDOM BALL IMAGE
-  selected_image = ballImage_array[random_index];
-  // DISPLAY BALL IMAGE
-  document.getElementById('ballImage').src = selected_image;
+  let random_index = Math.floor(Math.random() * ballImage_array.length);
+  let selected_image = ballImage_array[random_index];
+  //document.getElementById('ballImage').src = selected_image;
   //document.getElementById('ballImage').src = `img/${selected_image}`;
-  //document.getElementById('ballImage').src = `${selected_image}`
+  document.getElementById('ballImage').src = '../img/01.png'
 }
 
 //REMOVE IMAGE FUNCTION
 function removeImage(){
-  let remove = document.getElementById('ballImage');
-  remove.removeChild(remove.childNodes[0]);
+  selected_image.style.display = '';
+}
+
+//RESTART FUNCTION
+restartButton.addEventListener('click', function() {
+  toggleFormUserInfo();
+  setInputValues('', '', '');
+  removeImage();
+})
+
+//SUBMIT QUESTION FUNCTION
+form.addEventListener('submit', function(event) {
+event.preventDefault();
+let question = form[0].value;
+displayUserInfo(question);
+toggleFormUserInfo();
+get_random_image();
+})
+
+//ADD INPUT FUNCTION
+function addInput(type, placeholder) {
+let input = document.createElement(type);
+form.appendChild(input);
+input.placeholder = placeholder;
+}
+
+//ADD BUTTON FUNCTION
+function addButton(text){
+let button = document.createElement('button');
+form.appendChild(button);
+button.innerHTML = text;
+return button;
+}
+
+//DISPLAY QUESTION ON SCREEN
+function displayUserInfo(question) {
+repeatQuestion.innerText = question;
+}
+
+
+function setInputValues(question) {
+form[0].value = question;
+}
+
+
+function toggleFormUserInfo() {
+
+if (form.style.display === 'none') {
+    form.style.display = 'block';
+    questionElement.style.display = 'none';
+} else {
+    form.style.display = 'none';
+    questionElement.style.display = 'block';
+}
+
 }
