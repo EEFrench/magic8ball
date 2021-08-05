7c88681ca5370ff92c3f36e060dfde986c1bd084
EEFrench <edward.e.french@gmail.com>
2021-08-05 15:57:51 -0700

    Finished b-day

--- js/BDay.js
+++ js/BDay.js
@@ -7,3 +7,22 @@ const letters = document.querySelectorAll("li");
             }, index + 150);
         })
     }, 1000);
+
+const button = document.querySelector("#button");
+const icon = document.querySelector("#button > i");
+const audio = document.querySelector("audio");
+
+button.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.volume = 0.2;
+    audio.play();
+    icon.classList.remove('fa-volume-up');
+    icon.classList.add('fa-volume-mute');
+    
+  } else {
+    audio.pause();
+    icon.classList.remove('fa-volume-mute');
+    icon.classList.add('fa-volume-up');
+  }
+  button.classList.add("fade");
+});
